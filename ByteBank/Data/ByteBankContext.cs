f02d7c18df8f5958979ced03e518f1e1f12801bd
Marcelo Gomes <marcelogomes.0104@gmail.com>
2023-07-05 18:39:49 -0300

    change repository

---
+++ ByteBank/Data/ByteBankContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ByteBank.Data
+{
+    public class ByteBankContext : DbContext
+    {
+        public ByteBankContext(DbContextOptions options)
+            :base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Data Source=localhost,1435;User ID=sa;Password=1q2w3e4r;Trust Server Certificate=true");
+        }
+    }
+}
